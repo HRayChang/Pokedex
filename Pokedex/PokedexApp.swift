a8320f10900c16482b577bf06d57fa20bab655a4
RayChang <raychang861205@gmail.com>
2024-03-09 09:24:53 +0800

    start Pokedex project

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Ray Chang on 2024/3/9.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
