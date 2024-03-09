4e073f435e1b7db3999f17ee5bd6c2ed73a2b4cf
RayChang <raychang861205@gmail.com>
2024-03-09 09:43:50 +0800

    create Pokemon model and sample Pokemon

---
+++ Pokedex/Models/PokemonModel.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonModel.swift
+//  Pokedex
+//
+//  Created by Ray Chang on 2024/3/9.
+//
+
+import Foundation
+
+struct PokemonPage: Codable {
+    let count: Int
+    let next: String
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable, Identifiable, Equatable {
+    let id = UUID()
+    let name: String
+    let url: String
+    
+    static var samplePokemon = Pokemon(name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/")
+}
+
+struct DetailPokemon: Codable {
+    let id: Int
+    let height: Int
+    let weight: Int
+}
